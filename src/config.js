c5f6df1edd8d19aca62be01d8e137976e7b6184f
Vasily Myazin <vm@konspiredesign.com>
2024-07-03 18:05:51 -0300

    Add meta og image

--- src/config.js
+++ src/config.js
@@ -1,12 +1,15 @@
 // src/config.js
+const prodUrl = "https://investplan.rapidsystemshub.com/";
+const ogImage = "og-image.jpg";
+
 const config = {
   siteName: "Investment Plan Dashboard",
   description: "Plan and visualize your investment strategy focused on receiving dividends. Adjust your total investment and risk tolerance to see how it affects your portfolio allocation and potential returns.",
   heading: "Investment Plan Dashboard",
-  url: "https://investplan.rapidsystemshub.com/",
-  ogImage: "/banner.jpg",
+  url: prodUrl,
+  ogImage,
   twitterCreator: "@rapidsystemshub",
-  twitterImage: "https://www.joshwcomeau.com/images/og-interactive-guide-to-flexbox.png",
+  twitterImage: prodUrl + 'images/' + ogImage,
 };
 
 export default config;
